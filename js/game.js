5baf4b5a36ca6e2ca15d53ef0feb87e4d39c6d7d
h16k <95564547+h16k@users.noreply.github.com>
2023-01-28 02:49:00 +0900

    開始後に開始ボタン非表示、canvas高画質化

--- js/game.js
+++ js/game.js
@@ -1,15 +1,31 @@
+"use strict";
+
+
 //canvas情報を取得
 let canvas = document.getElementById("myCanvas");
 let ctx = canvas.getContext("2d");
 
+//キャンバス自体のサイズを設定する
+canvas.width = 6000;
+canvas.height = 4800;
+
+//実際に表示されるキャンバスのサイズを指定する
+canvas.style.width = "600px";
+canvas.style.height = "480px";
+
+
+
 //ボールの大きさを設定
-let ballRadius = 15;
-let rectSize = 130;
-let ballSpeed = 3;
+let ballRadius = 150;
+let rectSize = 1300;
+let ballSpeed = 30;
+
+let level = [1, 2, 3];
+let levelText = ["easy","normal","hard"];
 
 let midx = canvas.width / 2;
 let midy = canvas.height / 2;
-let margin = 50;
+let margin = ballRadius * 2;
 
 //開始時のボールの座標
 let spawn = [[margin, midy], [midx, margin], [canvas.width - margin, midy], [midx, canvas.height - margin]];
@@ -35,15 +51,26 @@ function init(){
     count++;
 }
 
+
+function getLevel(){
+    let radioButtonsForLevel = document.getElementsByName('level');
+    for (let i in level) {
+		if (radioButtonsForLevel.item(i).checked) {
+			return radioButtonsForLevel.item(i).value;
+		}
+	}
+}
+
 function startGame(){
-    answer = randNum(5);
+    changeElementVisibilityById("startGame");
+    answer = randNum(getLevel() * 5);
     timer = setInterval(draw,30);
 
 }
 
 function quizText(){
     let text = "How many balls are in the box?";
-    let fontSize = 36 ;
+    let fontSize = 360 ;
 
     //文字のスタイル（大きさ、フォント）を指定
 	ctx.font = 'bold ' + fontSize + 'px serif';
@@ -83,7 +110,7 @@ function draw(){
         s_wait--;
     }
 
-    if (((canvas.width / 2 - 3) < x) && (x < (canvas.width / 2) + 3) && ((canvas.height / 2) - 3 < y) && (y < (canvas.height / 2) + 3)) {
+    if (((canvas.width / 2 - ballRadius) < x) && (x < (canvas.width / 2) + ballRadius) && ((canvas.height / 2) - ballRadius < y) && (y < (canvas.height / 2) + ballRadius)) {
         if(e_wait){
             e_wait--;
             x -= dx;
@@ -103,4 +130,23 @@ function randNum(max) {
 	return Math.floor(Math.random() * max);
 }
 
-init();
+function changeElementVisibilityById(id){
+    document.getElementById(id).classList.toggle('hidden');
+}
+
+window.onload = () => {
+    init();
+	for (let i in level) {
+		let levelRadioButton = document.getElementById('levelRadioButton');
+		levelRadioButton.innerHTML +=
+			`<input type="radio" 
+		id = "${levelText[i]}" 
+		name="level" 
+		value= ${level[i]}
+		onclick="changeLevel();">
+	<label for = "${level[i]}">${levelText[i]}</label>`;
+	};
+
+	let defaultLevel = document.getElementById(`${levelText[1]}`);
+	defaultLevel.checked = true;
+};
